29b2ae65c84dfd5234a99762ac4c863040605f09
Marc Risney <mrisney@steelhouse.com>
2016-04-03 23:53:36 -0700

    added wrexegrams, and libary

---
+++ YoChing/CoinToss/WrexagramLibrary.swift
@@ -0,0 +1,83 @@
+//
+//  WrexagramLibrary.swift
+//  YoChing
+//
+//  Created by Marc Risney on 4/3/16.
+//  Copyright © 2016 Gary.com. All rights reserved.
+//
+
+import Foundation
+
+
+public func getOutcome(hexNum: Int) -> String {
+    
+    var outcome: String
+    
+    switch (hexNum) {
+    
+    case 111111: outcome = "wrex01"
+    case 111112: outcome = "wrex43"
+    case 111121: outcome = "wrex14"
+    case 111122: outcome = "wrex34"
+    case 111211: outcome = "wrex09"
+    case 111212: outcome = "wrex05"
+    case 111221: outcome = "wrex26"
+    case 111222: outcome = "wrex11"
+    case 112111: outcome = "wrex10"
+    case 112112: outcome = "wrex58"
+    case 112121: outcome = "wrex38"
+    case 112122: outcome = "wrex54"
+    case 112211: outcome = "wrex61"
+    case 112212: outcome = "wrex60"
+    case 112221: outcome = "wrex41"
+    case 112222: outcome = "wrex19"
+    case 121111: outcome = "wrex13"
+    case 121112: outcome = "wrex49"
+    case 121121: outcome = "wrex30"
+    case 121122: outcome = "wrex55"
+    case 121211: outcome = "wrex37"
+    case 121212: outcome = "wrex63"
+    case 121221: outcome = "wrex22"
+    case 121222: outcome = "wrex36"
+    case 122111: outcome = "wrex25"
+    case 122112: outcome = "wrex17"
+    case 122121: outcome = "wrex21"
+    case 122122: outcome = "wrex51"
+    case 122211: outcome = "wrex42"
+    case 122212: outcome = "wrex03"
+    case 122221: outcome = "wrex27"
+    case 122222: outcome = "wrex24"
+    case 211111: outcome = "wrex44"
+    case 211112: outcome = "wrex28"
+    case 211121: outcome = "wrex50"
+    case 211122: outcome = "wrex32"
+    case 211211: outcome = "wrex57"
+    case 211212: outcome = "wrex48"
+    case 211221: outcome = "wrex18"
+    case 211222: outcome = "wrex46"
+    case 212111: outcome = "wrex06"
+    case 212112: outcome = "wrex47"
+    case 212121: outcome = "wrex64"
+    case 212122: outcome = "wrex40"
+    case 212211: outcome = "wrex59"
+    case 212212: outcome = "wrex29"
+    case 212221: outcome = "wrex04"
+    case 212222: outcome = "wrex07"
+    case 221111: outcome = "wrex33"
+    case 221112: outcome = "wrex31"
+    case 221121: outcome = "wrex56"
+    case 221122: outcome = "wrex62"
+    case 221211: outcome = "wrex53"
+    case 221212: outcome = "wrex39"
+    case 221221: outcome = "wrex52"
+    case 221222: outcome = "wrex15"
+    case 222111: outcome = "wrex12"
+    case 222112: outcome = "wrex45"
+    case 222121: outcome = "wrex35"
+    case 222122: outcome = "wrex16"
+    case 222211: outcome = "wrex20"
+    case 222212: outcome = "wrex08"
+    case 222221: outcome = "wrex23"
+    case 222222: outcome = "wrex02"
+    }
+}
