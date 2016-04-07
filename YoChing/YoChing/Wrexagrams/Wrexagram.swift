960fcd08eab4d7336415196c71eb6191f5fdf1c1
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-04-07 16:27:24 -0700

    Fixing Wrexagrlm struct

--- YoChing/YoChing/Wrexagrams/Wrexagram.swift
+++ YoChing/YoChing/Wrexagrams/Wrexagram.swift
@@ -16,16 +16,16 @@ struct Wrexagram {
     private let number: Int?
     
     init(title: String) {
-        self.init(title: title, subTitle: "", number: 0)
+        self.init(title: title, body: nil, number: nil)
     }
     
-    init(title: String, subTitle: String?, number: Int?) {
+    init(title: String, body: String?, number: Int?) {
         self.title = title
-        self.subTitle = subTitle
+        self.body = body
         self.number = number
     }
     
     static func fromJson(jsonString: String) -> Wrexagram {
-        return Wrexagram(title: "BREAK IT DOWN", subTitle: "When Shit's Popping Off, True Player BRING IT", number: 1)
+        return Wrexagram(title: "BREAK IT DOWN", body: "When Shit's Popping Off, True Player BRING IT", number: 1)
     }
 }
